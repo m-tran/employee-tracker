ba10e1caf3e365088e098972c8097fb8254e8b2b
Michelle Tran <mptran0101@berkeley.edu>
2020-08-13 18:48:10 -0700

    added id as PRIMARY KEY to schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,16 +3,19 @@ CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
 CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
 );
 
 CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT UNIQUE NOT NULL,
 );
 
 CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT UNIQUE NOT NULL,
